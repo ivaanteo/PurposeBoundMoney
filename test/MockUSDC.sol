7f097ba63ba14ca7aa2c0fd8c3718ff27d537471
Ivan <ivanteozhiming@gmail.com>
2023-10-03 12:19:09 +0800

    mock usdc

---
+++ test/MockUSDC.sol
@@ -0,0 +1,28 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDCClone is ERC20 {
+    constructor() ERC20("USDCClone", "USDC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+}
